6fcbc400a5639f536f5c038a7a6239838189b71f
DESKTOP-CIAEAFE\melody <chaimelo@usc.edu>
2019-01-18 14:12:39 -0800

    Finished HomeDisplay UI

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeDisplay from './client/components/HomeDisplay.js'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello </Text>
+        <HomeDisplay />
       </View>
     );
   }
