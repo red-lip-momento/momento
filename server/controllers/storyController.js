9ef694b2615f9173686212d9075f31c7798ca2a2
Jonathan Mavandi <jonathan.mavandi@gmail.com>
2019-01-19 11:36:42 -0800

    changed post / to get inner and outer pins

--- server/controllers/storyController.js
+++ server/controllers/storyController.js
@@ -3,16 +3,15 @@ const moment = require('moment');
 const story = {};
 
 story.getAllNearby = (req, res, next) => {
-  const {lat, lng} = req.body
+  const {lat, lng} = req.body;
   // Look for for all stories within 1 longitude and latitude
   const latUp = lat + .1;
   const latDown = lat - .1;
   const lngRight = lng + .1;
   const lngLeft = lng - .1;
-  console.log(latUp, latDown, lngRight, lngLeft)
   db.query('SELECT * FROM story WHERE lat BETWEEN $1 AND $2 AND lng BETWEEN $3 AND $4', [latDown, latUp, lngLeft,lngRight])
   .then(data => {
-    console.log('All data within .1 lat/long: ', data);
+    // console.log('All data within .1 lat/long: ', data);
     res.locals.allPins = data;
     next();
   })
@@ -22,21 +21,41 @@ story.getAllNearby = (req, res, next) => {
   })
 };
 
-story.getStoryData = (req, res, next) => {
+story.getInnerAndOuterPins = (req, res, next) => {
+  const {lat, lng} = req.body;
+  const latUp = lat + .01;
+  const latDown = lat - .01;
+  const lngRight = lng + .01;
+  const lngLeft = lng - .01;
+  // checking if values are between these inner lats
+  const {allPins} = res.locals;
+  const innerPins = [];
+  const outerPins = [];
 
-  const { storyId } = req.body;
-  // Query the db for all data about one story
-  db.any('SELECT * FROM story WHERE _id=$1',[storyId])
-    .then(data =>  {
-      res.locals.storyId = data;
-      console.log('Success, got ', data);
-      next();
-    })
-    .catch(function(error) {
-      console.log('Error in getStoryData: ', error);
-      next(error);
-    });
-};
+  allPins.forEach( el => {
+    if (el.lat < latUp && el.lat > latDown && el.lng < lngRight && el.lng > lngLeft) innerPins.push(el);
+    // console.log(`${el.lat} < ${latUp} ${el.lat} > ${latDown} ${el.lng} < ${lngRight} ${el.lng} > ${lngLeft}`)
+    else outerPins.push(el);
+  })
+  console.log('innerPins: ',innerPins);
+  console.log('outerPins: ',outerPins);
+  res.locals.innerPins = innerPins;
+  res.locals.outerPins = outerPins;
+  next();
+}
+//   const { storyId } = req.body;
+//   // Query the db for all data about one story
+//   db.any('SELECT * FROM story WHERE _id=$1',[storyId])
+//     .then(data =>  {
+//       res.locals.storyId = data;
+//       console.log('Success, got ', data);
+//       next();
+//     })
+//     .catch(function(error) {
+//       console.log('Error in getStoryData: ', error);
+//       next(error);
+//     });
+// };
 
 story.create = (req, res, next) => {
   const {title, story, lat, lng } = req.body;
