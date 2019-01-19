165ba34a3170a2ef7d21e97cd11785f99a99ed23
Adrian Diamond <adriandiamond@Adrians-MacBook-Air.local>
2019-01-18 19:38:35 -0800

    Added Fetch button in App.js and getUserLocation handler.

--- App.js
+++ App.js
@@ -4,13 +4,43 @@ import HomeDisplay from './client/components/HomeDisplay.js'
 import CreateDisplay from './client/components/CreateDisplay.js'
 import ViewDisplay from './client/components/ViewDisplay.js';
 
+import FetchLocation from './client/components/FetchLocation.js';
+
 
 export default class App extends React.Component {
+  
+  state={
+    userLatitude: null,
+    userLongitude:null
+  }
+
+  //Handler for getting user location
+getUserLocationHandler = () => {
+  navigator.geolocation.getCurrentPosition(position => {
+    console.log(position)
+    this.setState({
+      userLatitude: position.coords.latitude,
+      userLongitude: position.coords.longitude
+    })
+    
+    // this.setState({
+    //   userLocation:{
+    //     latitude: position.coords.latitude,
+    //     longitude: position.coords.longitude,
+    //     latitudeDelta: 0.05,
+    //     longitudeDelta: 0.05
+    //   }
+    // });
+  }, err => console.log(err));
+}
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello </Text>
-        {/* <HomeDisplay /> */}
+        <Text>Current Lat is {this.state.userLatitude} Current Long is {this.state.userLongitude}</Text>
+        <FetchLocation onGetLocation={this.getUserLocationHandler}/>
+        {/* <HomeDisplay userLocation={this.userLocation}/> */}
         {/* <CreateDisplay/> */}
         <ViewDisplay/>
       </View>
