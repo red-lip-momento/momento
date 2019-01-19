364d3e8bfddb58103348215267afb72f1a0e4791
DESKTOP-CIAEAFE\melody <chaimelo@usc.edu>
2019-01-19 12:56:02 -0800

    added some things for fetching all markers

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
     title:'',
     story:'',
     userLocation: null,
-    usersPlaces: [],
+    allMarkers: {},
     viewHome: true,
     viewCreate: false,
     viewMemento: false,
@@ -42,6 +42,7 @@ getUserLocationHandler = () => {
 }
 componentDidMount() {
   this.getUserLocationHandler();
+
   console.log(Constants.manifest.packagerOpts);
 }
 
@@ -117,25 +118,51 @@ submit = () => {
   .catch(error => console.log('Error:', JSON.stringify(error)));
 }
 
+//Event Handler to get all markers to display on map
+getAllStories = () => {
+  let collection ={};
+  collection.lat = this.state.userLocation.latitude;
+  collection.lng = this.state.userLocation.longitude;
+
+  var url = 'http://192.168.0.95:3000/story/all';
+  fetch(url, {
+    method: 'POST', // or 'PUT'
+    headers:{
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(collection), // data can be `string` or {object}!
+  }).then(res => res.json())
+  .then(response => {
+    this.setState({
+      allMarkers: response
+    })
+  })
+  .catch(error => console.log('Error:', JSON.stringify(error)));
+}
+
 
 
   render() {
     //Conditional Render statement
     let componentArr = [];
     if(this.state.viewHome){
-      componentArr.push(<HomeDisplay userLocation={this.state.userLocation} usersPlaces ={this.state.usersPlaces}
+      if(this.state.userLocation) this.getAllStories();
+      componentArr.push(<HomeDisplay 
+      userLocation={this.state.userLocation} 
+      allMarkers ={this.state.allMarkers}
       createMemento={this.createMemento}
       displayMemento={this.displayMemento}
       />)
     }else if(this.state.viewCreate){
-      componentArr.push(<HomeDisplay userLocation={this.state.userLocation} usersPlaces ={this.state.usersPlaces}
+      componentArr.push(<HomeDisplay userLocation={this.state.userLocation} allMarkers ={this.state.allMarkers}
         createMemento={this.createMemento}
         displayMemento={this.displayMemento}
         api={this.state.api}
         />)
       componentArr.push(<CreateDisplay displayHome={this.displayHome} updateValue={this.updateValue} submit={this.submit}/>)
     }else if(this.state.viewMemento){
-      componentArr.push(<HomeDisplay userLocation={this.state.userLocation} usersPlaces ={this.state.usersPlaces}
+      componentArr.push(<HomeDisplay userLocation={this.state.userLocation} allMarkers ={this.state.allMarkers}
         createMemento={this.createMemento}
         displayMemento={this.displayMemento}
         />)
