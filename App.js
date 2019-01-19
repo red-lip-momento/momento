5b89118273d9866f66dcc538876c599ded1287bc
DESKTOP-CIAEAFE\melody <chaimelo@usc.edu>
2019-01-18 18:21:33 -0800

    Flipped the view component, but the styling is strange. Used FlipCard

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeDisplay from './client/components/HomeDisplay.js'
 import CreateDisplay from './client/components/CreateDisplay.js'
+import ViewDisplay from './client/components/ViewDisplay.js';
+
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello </Text>
         {/* <HomeDisplay /> */}
-        <CreateDisplay/>
+        {/* <CreateDisplay/> */}
+        <ViewDisplay/>
       </View>
     );
   }
